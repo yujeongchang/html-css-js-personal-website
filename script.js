0f4247350cf104630f82c7cb130569f0bc20d277
Yujeong Chang <hiyujeong0806@gmail.com>
2024-12-04 20:07:25 +0900

    finished website tutorial - original

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    // ?? i have no idea .. ??
+    menu.classList.toggle("open"); // what does 'open' mean?   
+    icon.classList.toggle("open");
+}
